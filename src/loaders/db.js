b6973bf18737f79a2a75f575cf15b2ee29b34923
dingding-21 <phj8221@naver.com>
2022-06-29 15:06:45 +0900

    add: createCollections

--- src/loaders/db.js
+++ src/loaders/db.js
@@ -1,14 +1,28 @@
-const mongoose = require("mongoose");
-const config = require("../config");
+const mongoose = require('mongoose');
+const config = require('../config');
+const Post = require('../models/Post');
+const User = require('../models/User');
+const Comment = require('../models/Comment');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(config.mongoURI);
 
-    mongoose.set("autoCreate", true);
+    mongoose.set('autoCreate', true);
 
-    console.log("Mongoose Connected ...");
+    console.log('Mongoose Connected ...');
 
+    User.createCollection().then(function (collection) {
+      console.log('User Collection is created!');
+    });
+
+    Post.createCollection().then(function (collection) {
+      console.log('Post Collection is created!');
+    });
+
+    Comment.createCollection().then(function (collection) {
+      console.log('Comment Collection is created!');
+    });
   } catch (err) {
     console.error(err.message);
     process.exit(1);
