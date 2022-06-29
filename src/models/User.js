c9b0085f8477160c3966e47924bf916519d0b349
dingding-21 <phj8221@naver.com>
2022-06-29 16:04:39 +0900

    chore: change variable name

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        intro: {
-            type: String,
-            required: false
-        }
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    profile: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
